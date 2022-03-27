ebd308831c8cf5aff0938017b728449917d6c078
Julian Neuhaus <neuhaus.julian@gmail.com>
2022-03-27 18:16:45 +0200

    Return training from database via grpc

--- grpc-backend/src/grpc_protobuffer_server.ts
+++ grpc-backend/src/grpc_protobuffer_server.ts
@@ -1,15 +1,36 @@
-import { sendUnaryData, ServerUnaryCall, Server, ServerCredentials } from '@grpc/grpc-js';
+import { sendUnaryData, ServerUnaryCall, Server, ServerCredentials, status} from '@grpc/grpc-js';
 import services from './proto/trainings_grpc_pb';
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
-import { Training } from './proto/trainings_pb';
+import { Training as ProtoTraining } from './proto/trainings_pb'
+import { Training as PrismaTraining } from '@prisma/client';
+import { DatabaseClient } from './database_client';
 export class GrpcProtobufferServer {
 
-    getTraining(call: ServerUnaryCall<Empty, Training>, callback: sendUnaryData<Training>) {
-        const training = new Training();
-        training.setId(1);
-        training.setName('TestTraining');
-        training.setDescription('Description');
-        callback(null, training);
+    private databaseClient!: DatabaseClient;
+
+    public GrpcProtobufferServer(databaseClient: DatabaseClient) {
+        this.databaseClient = databaseClient
+    }
+
+    getTraining(call: ServerUnaryCall<Empty, ProtoTraining>, callback: sendUnaryData<ProtoTraining>) {
+        const id = 1
+        this.databaseClient.getTraining(id)
+            .then((prismaTraining: PrismaTraining|null) => {
+                if(prismaTraining != null) {
+                    const training = this.convertToProtoTraining(prismaTraining);
+                    callback(null, training);
+                } else {
+                    callback(null, null);
+                }        
+            })
+            .catch(err => {
+                console.error('Unable to load training from database for id: ' + id, err)
+                
+                callback({
+                    code: status.INTERNAL,
+                    message: 'Unable to load training from database for id: ' + id
+                }, null);
+            })
     }
 
     public start() {
@@ -18,5 +39,13 @@ export class GrpcProtobufferServer {
         server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
             server.start();        
         });
+    }
+
+    private convertToProtoTraining(prismaTraining: PrismaTraining): ProtoTraining {
+        const training = new ProtoTraining()
+        training.setId(prismaTraining.id)
+        training.setName(prismaTraining.name)
+        training.setDescription(prismaTraining.description != null ? prismaTraining.description : '')
+        return training;
     }
 }
