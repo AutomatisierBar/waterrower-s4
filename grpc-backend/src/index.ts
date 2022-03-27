47beb3245ba18496b28edb24b370fc7ec6743d19
Julian Neuhaus <neuhaus.julian@gmail.com>
2022-03-27 17:30:14 +0200

    Removed express

--- grpc-backend/src/index.ts
+++ grpc-backend/src/index.ts
@@ -1,16 +1,4 @@
-import express from "express"
-import { DatabaseClient } from "./database_client";
 import { GrpcProtobufferServer } from "./grpc_protobuffer_server";
 
-const app = express()
 const grpc_server = new GrpcProtobufferServer();
 grpc_server.start();
-const port = 9999;
-app.set('json spaces', 2)
-
-const databaseClient = new DatabaseClient();
-
-// start the express server
-app.listen( port, () => {
-    console.log('server started at http://localhost:' + port);
-});
